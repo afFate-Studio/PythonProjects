8a9a599f7f23c35f76a5d57bea97bb9e0487131e
afFate-Studio <56927570+afFate-Studio@users.noreply.github.com>
2023-06-13 19:24:48 +0000

    Changing some loop stuff around, no real improvement

--- 100 Days of Python/Day 7/main.py
+++ 100 Days of Python/Day 7/main.py
@@ -4,7 +4,7 @@ import pyinputplus as pyip
 # word list for the game
 word_list = ["aardvark", "baboon", "camel"]
 
-word = word_list[randint(0, len(word_list))] # dynamic randint based on the word_list used to choose a word from the list
+word = word_list[randint(0, len(word_list)-1)] # dynamic randint based on the word_list used to choose a word from the list
 game_word = []      # empty list used to keep track of if the user guessed the correct value
 user_guesses = [] # empty list to keep track of user guesses
 
@@ -24,11 +24,17 @@ while True:
         checks user guess and then replaces the '_' in game_word with the letter
         if the guess was correct.
     """
-    for i in range(len(word)):
-        if user_guess == user_guesses[i]:
-            print("You have already guessed that.")
-        elif user_guess == word[i]:
-                game_word[i] = user_guess
+    if len(user_guesses) != 0:
+        for count, var in enumerate(user_guesses):
+            if user_guess == user_guesses[count]:
+                print("You have already guessed that.")
+                break
+    
+    for count, var in enumerate(word):
+        if user_guess == word[count]:
+            game_word[count] = user_guess
+    user_guesses.append(user_guess)
+   
         
 
     print(''.join(map(str, game_word))) # makes the list a single word and prints it
