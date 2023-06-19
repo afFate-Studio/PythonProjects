5938bb7c405cc93139840da4fcb854f29e3c60fb
afFate-Studio <56927570+afFate-Studio@users.noreply.github.com>
2023-06-19 14:11:35 +0000

    Added a switch case for the lives/gameboard

--- 100 Days of Python/Day 7/main.py
+++ 100 Days of Python/Day 7/main.py
@@ -7,7 +7,7 @@ word_list = ["aardvark", "baboon", "camel"]
 word = word_list[randint(0, len(word_list)-1)] # dynamic randint based on the word_list used to choose a word from the list
 game_word = []      # empty list used to keep track of if the user guessed the correct value
 user_guesses = [] # empty list to keep track of user guesses
-
+LIVES = 6
 """
     iterates over the length of the chosen word and then appends the correct amount
     of '_' to game_word list
@@ -35,6 +35,64 @@ while True:
             game_word[count] = user_guess
     user_guesses.append(user_guess)
    
-        
+    if ''.join(map(str, game_word)) == word:
+        print('You win!')
+        break
 
-    print(''.join(map(str, game_word))) # makes the list a single word and prints it
+    else:
+        LIVES -= 1
+        if LIVES >= 0:
+            print(''.join(map(str, game_word))) # makes the list a single word and prints it 
+        # switch statement printing the new gameboard every turn
+        match LIVES:
+                case 5:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print('      |')
+                    print('      |')
+                    print('      |')
+                    print('=======')
+                case 4:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print(' |    |')
+                    print('      |')
+                    print('      |')
+                    print('=======')
+                case 3:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print('/|    |')
+                    print('      |')
+                    print('      |')
+                    print('=======')
+                case 2:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print('/|\   |')
+                    print('      |')
+                    print('      |')
+                    print('=======')
+                case 1:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print('/|\   |')
+                    print('/     |')
+                    print('      |')
+                    print('=======')
+                case 0:
+                    print(' +----+')
+                    print(' |    |')
+                    print(' O    |')
+                    print('/|\   |')
+                    print('/ \   |')
+                    print('      |')
+                    print('=======')
+                    print(f'You lose here is the word: {word}')
+                    break
+            
