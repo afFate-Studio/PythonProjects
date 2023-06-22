8c07c4d170bde8d5002e4cb3d2d64474df58a61b
afFate-Studio <56927570+afFate-Studio@users.noreply.github.com>
2023-06-22 14:18:46 -0400

    Update main.py

--- 100 Days of Python/Day 8/main.py
+++ 100 Days of Python/Day 8/main.py
@@ -4,7 +4,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 def encrypt(text, shift):
     cypher_word = []
     for i, val in enumerate(text):
@@ -14,11 +14,18 @@ def encrypt(text, shift):
             index = alphabet.index(val)
         cypher_word.append(alphabet[shift+index])
     print(''.join(cypher_word))
-    
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+encrypt(plain_text=text, shift_amount=shift)
+
 encrypt(text=text,shift=shift)
